99922290bbf49a36291227f6c20f2a8f2407a840
Aditya Garg <adityagargmit@gmail.com>
2019-04-24 17:24:43 -0700

    mod sub.h

---
+++ Sub.h
@@ -0,0 +1,25 @@
+#ifndef SUB_H_
+#define SUB_H_
+#include <string>
+#include "base.h"
+using namespace std;
+
+class Sub : public Base{
+
+private:
+        Base* left;
+        Base* right;
+public:
+        Sub(Base* test1,Base* test2) {
+        left = test1;
+        right = test2;
+        }
+        double evaluate() {
+        return (left->evaluate() - right->evaluate());
+        }
+        string stringify() {
+        return left->stringify() + "-" + right->stringify();
+        }
+};
+#endif
+
